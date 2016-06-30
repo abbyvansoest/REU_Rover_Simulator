32076c67c055a10bd084cfc2241b7e8d9ea6bf31
Tobi Ogunyale <tobiogunyale@hotmail.com>
2016-06-30 13:43:03 -0700

    Add files via upload

---
+++ Home.cpp
@@ -0,0 +1,29 @@
+/*
+ * Home.cpp
+ *
+ *  Created on: Jun 30, 2016
+ *      Author: tobi
+ */
+
+#include "Home.h"
+
+Home::Home(double x, double y ) {
+	// TODOd Auto-generated constructor stub
+	x_cord =x;
+	y_cord =y;
+	capacity_value =0;
+}
+// Returns the value of the Position in the form of X_cord and Y_cords
+double Home::getPosition(){
+		return x_cord, y_cord;
+	}
+// Returns the current value of the capacity_value
+double Home:: getCurrentamount(){
+		return capacity_value;
+	}
+
+// Receives an amount from an agent and adds it to the current value of the capacity_value
+void Home::recieveValues(int newLoad){
+		capacity_value += newLoad;
+	}
+
---
+++ Home.h
@@ -0,0 +1,24 @@
+/*
+ * Home.h
+ *
+ *  Created on: Jun 30, 2016
+ *      Author: tobi
+ */
+
+#ifndef HOME_H_
+#define HOME_H_
+
+class Home {
+private :
+	double x_cord;
+	double y_cord;
+	int capacity_value;
+public:
+	Home(double,double);
+	double getPosition();
+	double getCurrentamount();
+	void recieveValues(int);
+
+};
+
+#endif /* HOME_H_ */
