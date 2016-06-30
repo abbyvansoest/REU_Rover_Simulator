862d8d8539305cdb3f87c0fe604d5b7b7323bb10
abbyvansoest <asoest@princeton.edu>
2016-06-30 14:13:31 -0700

    Update Home.cpp

--- Home.cpp
+++ Home.cpp
@@ -9,21 +9,21 @@
 
 Home::Home(double x, double y ) {
 	// TODOd Auto-generated constructor stub
-	x_cord =x;
-	y_cord =y;
-	capacity_value =0;
+	x_cord = x;
+	y_cord = y;
+	capacity_value = 0;
 }
 // Returns the value of the Position in the form of X_cord and Y_cords
-double Home::getPosition(){
-		return x_cord, y_cord;
-	}
+double Home::getPosition() {
+	return x_cord, y_cord;
+}
 // Returns the current value of the capacity_value
-double Home:: getCurrentamount(){
-		return capacity_value;
-	}
+double Home:: getCurrentAmount() {
+	return capacity_value;
+}
 
 // Receives an amount from an agent and adds it to the current value of the capacity_value
-void Home::recieveValues(int newLoad){
-		capacity_value += newLoad;
-	}
+void Home::recieveValues(int newLoad) {
+	capacity_value += newLoad;
+}
 
