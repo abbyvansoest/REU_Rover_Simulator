47450e422f709c7743d03ad21575968882662253
Tobi Ogunyale <tobiogunyale@hotmail.com>
2016-06-30 14:44:31 -0700

    An updated Home(Home Base) Class
    incorporates the position class

--- Home.h
+++ Home.h
@@ -7,16 +7,37 @@
 
 #ifndef HOME_H_
 #define HOME_H_
+#include <iostream>
+#include  "position.h"
 
 class Home {
 private :
-	double x_cord;
-	double y_cord;
 	int capacity_value;
+	Position Home_Pos;
 public:
-	Home(double,double);
-	double getPosition();
-	double getCurrentAmount();
+	Home(Position);
+
+	/*
+	 * Returns the value of the position of the base
+	 */
+	Position getPosition();
+
+
+	/*
+	 * Sets the Position of Home Base , by accepting a  position value
+	 */
+	void setPosition(Position pos);
+
+
+	/*
+	 * returns the current amount stored in the base
+	 */
+	double getCurrentamount();
+
+
+	/*
+	 * Receives an int value from an agent and adds the value held by the agent to the  capacity
+	 */
 	void recieveValues(int);
 
 };
